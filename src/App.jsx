83dcb0d011d11ed025d9540c5c7ff56aa3133387
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-11 09:52:44 +0300

    css file imported

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import AppHeader from './components/appHeader/appHeader';
 import AppList from './components/appList/appList';
 import AppAddTodo from './components/appAddTodo/appAddTodo';
 
+// app styles
+import './app.css';
+
 class App extends Component {
   state = {};
   render() {
