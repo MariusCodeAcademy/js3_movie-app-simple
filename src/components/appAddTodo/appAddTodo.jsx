8d57386f1ec0b7435721ad7c79e63ea448075634
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-11 09:47:20 +0300

    empty components done and in place

---
+++ src/components/appAddTodo/appAddTodo.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class AppAddTodo extends Component {
+  state = {};
+  render() {
+    return <h2>I am Add Todo</h2>;
+  }
+}
+
+export default AppAddTodo;
