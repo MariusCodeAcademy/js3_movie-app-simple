acc400736e22441ab4a5529ad43bfeb0b20e417b
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-15 11:04:25 +0300

    Link instead of a. No refresh component switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
 import Navbar from './components/navbar/navbar';
 import TodoPage from './components/todoPage/todoPage';
+import AboutPage from './components/pages/about';
+import ContactsPage from './components/pages/contact';
+import HomePage from './components/pages/home';
 
 // app styles
 import './app.css';
@@ -10,7 +14,17 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <TodoPage />
+        <div className="container">
+          {/* sutikes pirma atitikusi route jis nebeiesko toliau  */}
+          <Switch>
+            <Route path="/todos" component={TodoPage}></Route>
+            <Route path="/about" component={AboutPage}></Route>
+            <Route path="/contact" component={ContactsPage}></Route>
+            <Route path="/" component={HomePage}></Route>
+          </Switch>
+
+          {/* <Route path="/" exact component={HomePage}></Route> */}
+        </div>
       </div>
     );
   }
