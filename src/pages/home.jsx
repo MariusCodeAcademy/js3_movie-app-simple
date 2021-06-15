c3045891eeeaf9c1f2f32c85403a1596c64be094
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-15 14:37:29 +0300

    homePageComponent

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>This is home page</h1>
+        <p>Hommie cosy</p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
