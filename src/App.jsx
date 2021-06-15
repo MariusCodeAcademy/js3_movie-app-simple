d5eeb1cc97cb03f215cb47fa6fbc616e4af6691d
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-15 11:24:22 +0300

    route params works

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TodoPage from './components/todoPage/todoPage';
 import AboutPage from './components/pages/about';
 import ContactsPage from './components/pages/contact';
 import HomePage from './components/pages/home';
+import SingleTodo from './components/pages/singleTodo';
 
 // app styles
 import './app.css';
@@ -17,6 +18,7 @@ class App extends Component {
         <div className="container">
           {/* sutikes pirma atitikusi route jis nebeiesko toliau  */}
           <Switch>
+            <Route path="/todos/:id" component={SingleTodo}></Route>
             <Route path="/todos" component={TodoPage}></Route>
             <Route path="/about" component={AboutPage}></Route>
             <Route path="/contact" component={ContactsPage}></Route>
