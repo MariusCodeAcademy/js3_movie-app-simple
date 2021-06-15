e940276e6dedbfe8deda6b6e4d0afed849860961
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-15 09:43:20 +0300

    navbar styles

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+// importuojam modulini individualu css
+import navStyles from './navbar.module.css';
 
 class Navbar extends Component {
   state = {};
   render() {
-    return <h2>Navbar</h2>;
+    return (
+      <nav className={navStyles.nabar}>
+        <a className="logo" href="/">
+          ReactApp
+        </a>
+        <div className="nav-links">
+          <a className="nav-link" href="/">
+            Home
+          </a>
+          <a className="nav-link" href="/todos">
+            Todos
+          </a>
+          <a className="nav-link" href="/about">
+            About
+          </a>
+          <a className="nav-link" href="/contact">
+            About
+          </a>
+        </div>
+      </nav>
+    );
   }
 }
 
