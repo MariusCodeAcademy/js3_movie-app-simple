cf70ff98c2b18011b0ee5d814e28366102f278af
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-16 11:41:45 +0300

    add todo on enter

--- src/components/appAddTodo/appAddTodo.jsx
+++ src/components/appAddTodo/appAddTodo.jsx
@@ -16,12 +16,21 @@ class AppAddTodo extends Component {
     this.setState({ newTodo: '' });
   };
 
+  handleEnter = (e) => {
+    // console.log(e.keyCode); // 13 === enter
+    // if (e.keyCode === 13) this.sendAddTodo();
+
+    // JeiSalygaTrue && vygdomSita
+    e.keyCode === 13 && this.sendAddTodo();
+  };
+
   render() {
     return (
       <div className="add-todo-container">
         <i onClick={this.sendAddTodo} className="fa fa-plus-circle add-icon"></i>
         <input
           onChange={this.handleChange}
+          onKeyUp={this.handleEnter}
           value={this.state.newTodo}
           type="text"
           placeholder="Add new Todo"
