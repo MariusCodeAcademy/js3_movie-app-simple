5d5a37f82ccd5cb92691ca5759f4f6d48df1d9dc
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-23 11:12:14 +0300

    uzduotys

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,4 +1,22 @@
-//
+// 1. paduoti reikalinga info i listGroup
+
+// 2. list group pasidaryti saraso jsx
+
+// 3. dinamiskai sugeneruoti tiek el kiek yra genres
+
+// 4. movie table sukurti metoda kuris nustato zanra
+
+// 5. iskviesti ta metoda is listGroup componento
+
+// 6. movieTable sukurti state savybe currentGenre
+
+// 7. pakeisti zanra paspaudus ant saraso el
+
+// 8. rodyti zanra kuris yra nustatytas melynam fone (pan kaip pagination)
+
+// 9. padaryti kad rodytu tik tuos filmus kurie attitinka pasirinkta zanra
+
+// 10. prideti 'All genres' pasirinkima
 
 const ListGroup = (props) => {
   return null;
