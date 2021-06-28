e1c9371e03827bed983d0fba27007331d7feba1a
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-28 08:22:21 +0300

    bootstrap responsive layout

---
+++ src/components/bootstrapTest.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+class BootTest extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        <h1>BootSt</h1>
+
+        <div className="row">
+          <div className="col-12 col-md-7">
+            <div className="">
+              <h3>Title1 </h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem totam, tempore
+                quibusdam natus amet perferendis odit a. Ea, nesciunt enim?
+              </p>
+            </div>
+          </div>
+          <div className="col">
+            <div className="">
+              <h3>Title2 </h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vitae cumque eligendi
+                ad? Et aliquam reiciendis ullam odio suscipit! Illum minus alias nam, obcaecati
+                eaque cum. Incidunt placeat animi soluta.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12 col-md-7">
+            <div className="row">
+              <div className="col">
+                <h3>Title1 </h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem totam,
+                  tempore quibusdam natus amet perferendis odit a. Ea, nesciunt enim?
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <h3>Title1 </h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem totam,
+                  tempore quibusdam natus amet perferendis odit a. Ea, nesciunt enim?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <h3>Title large </h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos aliquam tenetur iure ut
+              fugiat qui quisquam omnis, vero incidunt autem soluta impedit obcaecati aut natus
+              sunt. Dolor laudantium recusandae omnis id aut sequi, nihil voluptas dolorum soluta
+              eos quasi vitae facilis illum. Sunt asperiores corporis, quaerat explicabo voluptas
+              nam molestias! Voluptas unde aspernatur nesciunt distinctio eum expedita mollitia a
+              ipsa!
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BootTest;
